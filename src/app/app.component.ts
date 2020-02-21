c8857177f3910fcc1a1f26104ca23ed3f5d138b4
nginnx <rpg@rpg.com>
2020-02-21 15:31:10 +0800

    feat: feature

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-root',
+  styles: [],
+  template: `
+    <navigation></navigation>
+    <router-outlet></router-outlet>
+  `,
+})
+export class AppComponent {
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+//noinspection JSUnusedGlobalSymbols
+export const environment = {
+  production: true,
+
+  datasource: {
+    posts: './posts.json',
+    tags: './tags.json',
+    categories: './categories.json',
+    application: './application.json'
+  }
+};
